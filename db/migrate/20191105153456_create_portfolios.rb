0be18cb34e6f06f961868716a91a2960fc03f15f
Ryo Kitagawa <ryo.kitagawa@tabula.jp>
2019-11-06 01:05:56 +0900

    Created seeds

--- db/migrate/20191105153456_create_portfolios.rb
+++ db/migrate/20191105153456_create_portfolios.rb
@@ -1,7 +1,7 @@
 class CreatePortfolios < ActiveRecord::Migration[6.0]
   def change
     create_table :portfolios do |t|
-      t.sting :title
+      t.string :title
       t.string :subtitle
       t.text :body
       t.text :main_image
