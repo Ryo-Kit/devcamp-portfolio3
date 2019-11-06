de0b97712a417597987118e0ae558fa92730a680
Ryo Kitagawa <ryo.kitagawa@tabula.jp>
2019-11-06 17:47:11 +0900

    Implemented edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,25 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.new
   end
 
+  def edit
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_items = Portfolio.find(params[:id])
+   
+    respond_to do |format|
+
+    if @portfolio_items.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
   def create
     @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
 
